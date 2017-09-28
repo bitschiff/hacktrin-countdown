e45b6ee0a5cb7af719d13e9eef5eadda2b38e243
Eli <elihschiff@gmail.com>
2017-09-27 21:01:37 -0400

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,43 @@
+var endDate = new Date("Feb 12, 2018 15:37:25").getTime();
+
+var x = setInterval(function() {
+	document.getElementById("header").innerHTML = "HackTrin Countdown:";
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = endDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+ // document.getElementById("counter").innerHTML = numToBinary(days) + ":" + numToBinary(hours) + ":" + numToBinary(minutes) + ":" + numToBinary(seconds);
+ document.getElementById("days").innerHTML = numToBinary(days);
+ document.getElementById("hours").innerHTML = numToBinary(hours);
+ document.getElementById("mins").innerHTML = numToBinary(minutes);
+ document.getElementById("secs").innerHTML = numToBinary(seconds);
+
+ var splitters = document.getElementsByClassName("splitter");
+ for(i=0;i<splitters.length;i++){
+ 	splitters[i].innerHTML = ":";
+ }
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+  	clearInterval(x);
+  	document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+
+//document.getElementById("counter").innerHTML = numToBinary(165);
+
+function numToBinary(theNum) {
+	return (theNum >>> 0).toString(2);              
+}
